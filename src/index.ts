fa460503372c0bde1fd16642bd43cbf78eaa4002
Oleg Yapparov <oyapparov@gmail.com>
2020-11-18 23:54:17 +0100

    Add linting, data, endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import express from "express"
+import express from 'express'
+import bodyParser from 'body-parser'
+import { listings } from './listings'
+
 const app = express()
 const port = 9000
 
-app.get("/", (req, res) => res.send("Hello world!"))
+app.use(bodyParser.json())
+
+app.get("/", (_req, res) => res.send(`Hello world!`))
+app.get("/listings", (_req, res) => res.send(listings))
+app.post("/listings", (_req, res) => res.send(listings))
+app.delete("/listings", (req, res) => {
+    const id: string = req.body.id
+
+    for (let i = 0; i < listings.length; i++) {
+        if (listings[i].id === id) {
+            res.statusCode = 200
+            return res.send(listings.splice(i, 1))
+        }
+    }
+
+    res.statusCode = 404
+    res.send('Failed to delete listing')
+})
 
 app.listen(port)
 
