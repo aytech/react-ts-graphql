2461eb778157a878cd679aa69ce26a85c0b12f1a
Oleg Yapparov <oyapparov@gmail.com>
2020-12-20 12:03:53 +0100

    Add user resolvers

---
+++ server/src/graphql/resolvers/User/index.ts
@@ -0,0 +1,105 @@
+import { IResolvers } from "apollo-server-express";
+import { Request } from "express";
+import { isError } from "util";
+import { Database, User } from "../../../lib/types";
+import { authorize } from "../../../lib/utils";
+import { UserArgs, UserBookingsArgs, UserBookingsData, UserListingsArgs, UserListingsData } from "./types";
+
+export const userResolvers: IResolvers = {
+  Query: {
+    user: async (
+      _root: undefined,
+      { id }: UserArgs,
+      { db, req }: { db: Database, req: Request }
+    ): Promise<User> => {
+      try {
+        const user = await db.users.findOne({ _id: id })
+
+        if (!user) {
+          throw new Error(`User ${ id } can't be found`)
+        }
+
+        const viewer = await authorize(db, req)
+
+        if (viewer && viewer._id === user._id) {
+          user.authorized = true
+        }
+
+        return user
+
+      } catch (error) {
+        throw new Error(`Failed to query user ${ id }, message: ${ error }`)
+      }
+    }
+  },
+  User: {
+    id: (user: User): string => {
+      return user._id
+    },
+    hasWallet: (user: User): boolean => {
+      return Boolean(user.walletId)
+    },
+    income: (user: User): number | null => {
+      return user.authorized ? user.income : null
+    },
+    bookings: async (
+      user: User,
+      { limit, page }: UserBookingsArgs,
+      { db }: { db: Database }
+    ): Promise<UserBookingsData | null> => {
+      try {
+        if (!user.authorized) {
+          return null
+        }
+        const data: UserBookingsData = {
+          total: 0,
+          result: []
+        }
+
+        let cursor = await db.bookings.find({
+          _id: { $in: user.bookings }
+        })
+        // e.g.: page = 1; limit = 10; cursor will start at 0
+        // e.g.: page = 2; limit = 10; cursor will start at 10
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+
+      } catch (error) {
+        throw new Error(`Failed to query user bookings ${ error }`)
+      }
+    },
+    listings: async (
+      user: User,
+      { limit, page }: UserListingsArgs,
+      { db }: { db: Database }
+    ): Promise<UserListingsData | null> => {
+      try {
+        const data: UserListingsData = {
+          total: 0,
+          result: []
+        }
+
+        let cursor = await db.listings.find({
+          _id: { $in: user.listings }
+        })
+        // e.g.: page = 1; limit = 10; cursor will start at 0
+        // e.g.: page = 2; limit = 10; cursor will start at 10
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+
+      } catch (error) {
+        throw new Error(`Failed to query user listings ${ error }`)
+      }
+    }
+  }
+}
--- server/src/graphql/resolvers/index.ts
+++ server/src/graphql/resolvers/index.ts
@@ -1,4 +1,5 @@
-import merge from 'lodash.merge'
-import { viewerResolver } from './Viewer'
+import merge from "lodash.merge"
+import { viewerResolver } from "./Viewer"
+import { userResolvers } from "./User"
 
-export const resolvers = merge(viewerResolver)
+export const resolvers = merge(userResolvers, viewerResolver)
