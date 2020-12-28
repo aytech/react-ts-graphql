100c3a8ee43e13dbb864af73d8875b9dd3c5acc5
Oleg Yapparov <oyapparov@gmail.com>
2020-12-28 23:00:43 +0100

    server - get filtered listings

--- server/src/graphql/resolvers/Listing/index.ts
+++ server/src/graphql/resolvers/Listing/index.ts
@@ -3,7 +3,14 @@ import { Request } from "express";
 import { ObjectId } from "mongodb";
 import { Database, Listing, User } from "../../../lib/types";
 import { authorize } from "../../../lib/utils";
-import { ListingArgs, ListingBookingsArgs, ListingBookingsData } from "./types";
+import {
+  ListingArgs,
+  ListingBookingsArgs,
+  ListingBookingsData,
+  ListingsArgs,
+  ListingsData,
+  ListingsFilter
+} from "./types";
 
 export const listingResolvers: IResolvers = {
   Query: {
@@ -25,6 +32,41 @@ export const listingResolvers: IResolvers = {
         return listing
       } catch (error) {
         throw new Error(`Failed to query listing: ${ error }`)
+      }
+    },
+    listings: async (
+      _root: undefined,
+      { filter, limit, page }: ListingsArgs,
+      { db }: { db: Database }
+    ): Promise<ListingsData> => {
+      try {
+        const data: ListingsData = {
+          total: 0,
+          result: []
+        }
+
+        let cursor = await db.listings.find({})
+
+        if (filter && filter === ListingsFilter.PRICE_LOW_TO_HIGH) {
+          cursor = cursor.sort({ price: 1 })
+        }
+
+        if (filter && filter === ListingsFilter.PRICE_HIGH_TO_LOW) {
+          cursor = cursor.sort({ price: -1 })
+        }
+
+        // e.g.: page = 1; limit = 10; cursor will start at 0
+        // e.g.: page = 2; limit = 10; cursor will start at 10
+        cursor = cursor.skip(page > 0 ? (page - 1) * limit : 0)
+        cursor = cursor.limit(limit)
+
+        data.total = await cursor.count()
+        data.result = await cursor.toArray()
+
+        return data
+
+      } catch (error) {
+        throw new Error(`Failed to query listings ${ error }`)
       }
     }
   },
