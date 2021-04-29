0418f350c169cf61ebb932ac5e53795118b57a2e
Oleg Yapparov <oyapparov@gmail.com>
2021-04-29 23:12:33 +0200

    Listings in progress

--- client/src/sections/Listings/index.tsx
+++ client/src/sections/Listings/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Layout, List, Typography } from 'antd'
+import React, {useState} from 'react'
+import { Affix, Layout, List, Typography } from 'antd'
 import { useQuery } from '@apollo/react-hooks'
 import {
   Listings as ListingsData,
@@ -7,9 +7,14 @@ import {
 } from '../../lib/graphql/queries/Listings/__generated__/Listings'
 import { LISTINGS } from '../../lib/graphql/queries'
 import { ListingFilter } from '../../lib/graphql/globalTypes'
-import { ListingCard } from '../../lib/components'
+import { ErrorBanner, ListingCard } from '../../lib/components'
 import { RouteComponentProps } from 'react-router'
 import { Link } from 'react-router-dom'
+import { 
+  ListingsFilters, 
+  ListingsPagination,
+  ListingsSkeleton 
+} from './components'
 
 interface MatchParams {
   location: string
@@ -19,31 +24,65 @@ const { Paragraph, Text, Title } = Typography
 const PAGE_LIMIT = 8
 
 export const Listings = ({ match }: RouteComponentProps<MatchParams>) => {
-  const { data } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
+  const [ filter, setFilter ] = useState(ListingFilter.PRICE_LOW_TO_HIGH)
+  const [ page, setPage ] = useState(1)
+  const { loading, data, error } = useQuery<ListingsData, ListingsVariables>(LISTINGS, {
     variables: {
       location: match.params.location,
-      filter: ListingFilter.PRICE_LOW_TO_HIGH,
+      filter,
       limit: PAGE_LIMIT,
-      page: 1
+      page
     }
   })
+
+  if (loading) {
+    return (
+      <Content className="listings">
+        <ListingsSkeleton />
+      </Content>
+    )
+  }
+
+  if (error) {
+    return (
+      <Content className="listings">
+        <ErrorBanner description="We either could't find anything matching your search or encountered an error. If you're searching for a unique location, try searching again with more common keywords."/>
+        <ListingsSkeleton/>
+      </Content>
+    )
+  }
+
   const listings = data ? data.listings : null
   const listingsRegion = listings ? listings.region : null
   const listingsSectionElement = listings && listings.result.length ? (
-    <List
-      grid={ {
-        gutter: 8,
-        xs: 1,
-        sm: 3,
-        lg: 4
-      } }
-      dataSource={ listings.result }
-      renderItem={ listing => (
-        <List.Item>
-          <ListingCard listing={ listing } />
-        </List.Item>
-      ) }
-    />
+    <div>
+      <Affix offsetTop={ 64 }>
+      <ListingsPagination
+        total={ listings.total }
+        page={ page }
+        limit={ PAGE_LIMIT }
+        setPage={ setPage }
+      />
+      <ListingsFilters 
+        filter={ filter } 
+        setFilter={ setFilter }
+      />
+      </Affix>
+      <List
+        grid={ {
+          gutter: 8,
+          xs: 1,
+          sm: 3,
+          lg: 4
+        } }
+        dataSource={ listings.result }
+        renderItem={ listing => (
+          <List.Item>
+            <ListingCard listing={ listing } />
+          </List.Item>
+        ) }
+      />
+    </div>
   ) : (
     <div>
       <Paragraph>
